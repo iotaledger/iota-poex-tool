2b7fe5bb9ec838412cbfde7fc4da578c903d6065
Marwen Trabelsi <marwen.projob@gmail.com>
2019-04-30 16:23:05 +0200

    updated commented code typo

--- defaults.js
+++ defaults.js
@@ -2,7 +2,7 @@ const dSeed = 'HEQLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLO
 const dAddress = 'MRDVKCDQAPYQOJEQTUWDMNYZKDUDBRNHJWV9VTKTCUUYQICLPFBETMYYVKEPFCXZE9EJZHFUWJZVEWUCWSGDUVMOYD'
 const dProvider = 'https://altnodes.devnet.iota.org'
 const dDepth = 3
-const dMinWeightMagnitude = 9 // 14 for devenets
+const dMinWeightMagnitude = 9 // 14 for Mainnet
 const dTag = 'BLUEPRINT9'
 
 module.exports  = { dSeed, dAddress, dDepth, dMinWeightMagnitude, dProvider, dTag }

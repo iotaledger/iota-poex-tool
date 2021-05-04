999fffaf4ce24c25dcc007291c7582e44c4af39e
RubensGitHub <ruben.koch@iota.org>
2021-05-04 11:44:12 +0200

    finished initial sketch of fetch and publish on both C2 and legacy, adjusted cmd-application too. Tests and verify-operation left

--- src/cmd.js
+++ src/cmd.js
@@ -1,80 +1,102 @@
 #!/usr/bin/env node
 const program = require('commander')
 const lib = require('./lib')
-const { dSeed, dAddress, dDepth, dMinWeightMagnitude, dProvider, dTag } = require('./defaults')
+// const { config.legacyOptions.seed, config.legacyOptions.address, config.legacyOptions.depth, config.legacyOptions.minWeightMagnitude, config.legacyOptions.provider, config.legacyOptions.tag } = require('./defaults')
+
+const config = require("./../options.config");
 
 program
   .command('hash <file>')
   .option('-b, --binary', 'Binary input rather than Path')
   .action(function (file, options) {
     const hashVal = lib.hash(file, options.binary)
-    console.log(file, 'hash = ',hashVal)
+    console.log(file, 'hash = ', hashVal)
   })
 
-  program
-    .command('publish <data>')
-    .option('-p, --provider [provider]', `Provider, defaults: ${dProvider}`)
-    .option('-s, --seed [seed]', `Seed, defaults: ${dSeed}`)
-    .option('-a, --address [address]', `Provider, defaults: ${dAddress}`)
-    .option('-m, --magnitude [magnitude]', `MinWeightMagnitude, defaults: ${dMinWeightMagnitude}`)
-    .option('-d, --depth [depth]', `Depth, defaults: ${dDepth}`)
-    .option('-t, --tag [depth]', `Tag, defaults: ${dTag}`)
-    .option('-f, --full', 'Show full response from the Tangle, defaults: only show TX Hash')
-    .action(async (data, options) => {
-      const provider = options.provider ? options.provider : dProvider
-      const seed = options.seed ? options.seed : dSeed
-      const address = options.address ? options.address : dAddress
-      const depth = options.depth ? options.depth : dDepth
-      const tag = options.tag ? options.tag : dTag
-      const minWeightMagnitude = options.magnitude ? options.magnitude : dMinWeightMagnitude
+program
+  .command('publish <data>')
+  .option('-l, --legacy [legacy]', `Option to only use if operation should be performed on the legacy network`)
+  .option('-p, --provider [provider]', `Provider, defaults: ${config.legacyOptions.provider}`)
+  .option('-s, --seed [seed]', `Seed, defaults: ${config.legacyOptions.seed}`)
+  .option('-a, --address [address]', `Provider, defaults: ${config.legacyOptions.address}`)
+  .option('-m, --magnitude [magnitude]', `MinWeightMagnitude, defaults: ${config.legacyOptions.minWeightMagnitude}`)
+  .option('-d, --depth [depth]', `Depth, defaults: ${config.legacyOptions.depth}`)
+  .option('-t, --tag [depth]', `Tag, defaults: ${config.legacyOptions.tag}`)
+  .action(async (data, options) => {
+
+    if (!options.legacy) {
+      //Perform operation on Chrysalis-network
+      const provider = options.provider ? options.provider : config.chrysalisOptions.provider
+      const res = lib.publishC2(data, provider)
+      console.log('MessageId =', res)
+    }
+
+    else {
+      //Perform operation on legacy-network
+      const provider = options.provider ? options.provider : config.legacyOptions.provider
+      const seed = options.seed ? options.seed : config.legacyOptions.seed
+      const address = options.address ? options.address : config.legacyOptions.address
+      const depth = options.depth ? options.depth : config.legacyOptions.depth
+      const tag = options.tag ? options.tag : config.legacyOptions.tag
+      const minWeightMagnitude = options.magnitude ? options.magnitude : config.legacyOptions.minWeightMagnitude
       try {
-        const res = await lib.publish({ provider,
-                      data,
-                      seed,
-                      tag,
-                      address,
-                      minWeightMagnitude,
-                      depth
-                    })
-        if(options.full) {
-          console.log(res)
-        } else {
-          console.log('tx Hash =', res[0].hash)
-        }
-      } catch(e) {
+        const res = await lib.publishLegacy({
+          provider,
+          data,
+          seed,
+          tag,
+          address,
+          minWeightMagnitude,
+          depth
+        })
+
+        console.log('tx Hash =', res[0].hash)
+
+      } catch (e) {
         console.log(e)
       }
-
-    })
+    }
+  })
 
 
 program
   .command('fetch <hash>')
+  .option('-l, --legacy [legacy]', `Option to only use if operation should be performed on the legacy network`)
   .option('-a, --address <address>', 'Address of the channel')
   .option('-c, --convert', 'Convert to String after fetching')
-  .option('-p, --provider [provider]', `Provider, defaults: ${dProvider}`)
-  .option('-s, --seed [seed]', `Seed, defaults: ${dSeed}`)
-  .option('-m, --magnitude [magnitude]', `MinWeightMagnitude, defaults: ${dMinWeightMagnitude}`)
-  .option('-d, --depth [depth]', `Depth, defaults: ${dDepth}`)
+  .option('-p, --provider [provider]', `Provider, defaults: ${config.legacyOptions.provider}`)
+  .option('-s, --seed [seed]', `Seed, defaults: ${config.legacyOptions.seed}`)
+  .option('-m, --magnitude [magnitude]', `MinWeightMagnitude, defaults: ${config.legacyOptions.minWeightMagnitude}`)
+  .option('-d, --depth [depth]', `Depth, defaults: ${config.legacyOptions.depth}`)
   .action(async (hash, options) => {
-    const address = options.address ? options.address : dAddress
-    const provider = options.provider ? options.provider : dProvider
-    const seed = options.seed ? options.seed : dSeed
-    const depth = options.depth ? options.depth : dDepth
-    const minWeightMagnitude = options.magnitude ? options.magnitude : dMinWeightMagnitude
+    if (!options.legacy) {
+      //Perform operation on Chrysalis-network
       try {
-        const message = await lib.fetch({ provider,
-            seed,
-            address,
-            hash,
-            minWeightMagnitude,
-            depth
-          })
-        console.log('tx value =', message)
-        return message
-      } catch(e) {
+        const provider = options.provider ? options.provider : config.chrysalisOptions.provider
+        const res = lib.fetchC2(data, provider)
+        console.log('Payload-data =', res)
+      }
+      catch (e) {
         console.log(e)
       }
+    }
+    else {
+      //Perform fetch operation on legacy network
+      try {
+        const address = options.address ? options.address : config.legacyOptions.address
+        const provider = options.provider ? options.provider : config.legacyOptions.provider
+        const message = await lib.fetchLegacy({
+          provider,
+          address,
+          hash
+        })
+        console.log('tx value =', message)
+        return message
+      }
+      catch (e) {
+        console.log(e)
+      }
+    }
   })
 
 program
@@ -82,16 +104,16 @@ program
   .option('-b, --binary', 'Binary input rather than Path')
   .option('-a, --address <address>', 'Address of the channel')
   .option('-h, --hash <hash>', 'Transaction Hash of the channel')
-  .option('-p, --provider [provider]', `Provider, defaults: ${dProvider}`)
-  .option('-s, --seed [seed]', `Seed, defaults: ${dSeed}`)
-  .option('-m, --magnitude [magnitude]', `MinWeightMagnitude, defaults: ${dMinWeightMagnitude}`)
-  .option('-d, --depth [depth]', `Depth, defaults: ${dDepth}`)
+  .option('-p, --provider [provider]', `Provider, defaults: ${config.legacyOptions.provider}`)
+  .option('-s, --seed [seed]', `Seed, defaults: ${config.legacyOptions.seed}`)
+  .option('-m, --magnitude [magnitude]', `MinWeightMagnitude, defaults: ${config.legacyOptions.minWeightMagnitude}`)
+  .option('-d, --depth [depth]', `Depth, defaults: ${config.legacyOptions.depth}`)
   .action(async function (docpath, options) {
-    const provider = options.provider ? options.provider : dProvider
-    const seed = options.seed ? options.seed : dSeed
-    const depth = options.depth ? options.depth : dDepth
-    const address = options.address ? options.address : dAddress
-    const minWeightMagnitude = options.magnitude ? options.magnitude : dMinWeightMagnitude
+    const provider = options.provider ? options.provider : config.legacyOptions.provider
+    const seed = options.seed ? options.seed : config.legacyOptions.seed
+    const depth = options.depth ? options.depth : config.legacyOptions.depth
+    const address = options.address ? options.address : config.legacyOptions.address
+    const minWeightMagnitude = options.magnitude ? options.magnitude : config.legacyOptions.minWeightMagnitude
     try {
       const verified = await lib.verify({
         provider,
@@ -102,7 +124,7 @@ program
         minWeightMagnitude
       }, options.binary, docpath)
       console.log(verified)
-    } catch(e) {
+    } catch (e) {
       console.log(e)
     }
 
--- src/lib.ts
+++ src/lib.ts
@@ -1,29 +1,25 @@
 import { sha256 } from 'js-sha256';
 import fs from "fs";
-import { ClientBuilder } from "@iota/client";
-import { Converter } from '@iota/iota.js';
 import { utils } from "./utils";
 
 import { IConfiguration } from "./../models/IConfig";
 import { ILegacyPublishOptions } from "./../models/ILegacyPublishOptions";
 import { ILegacyFetchOptions } from "./../models/ILegacyFetchOptions";
 
-import { composeAPI, Transaction } from "@iota/core";
+import { composeAPI, Transaction, Transfer } from "@iota/core";
 import { asciiToTrytes, trytesToAscii } from "@iota/converter";
 
-
-//TODO: Most likely with C2, we should get rid of seed and addresses entirely
-
-//TODO: Be consistent with optional parameters that might get imported through config-file
+import { ClientBuilder } from "@iota/client";
+import { Converter } from '@iota/iota.js';
 
 
 /**
  * 
- * @param agnosticData 
- * @param isBinaryInput 
+ * @param agnosticData Either directly a binary file or a filepath to the file that should be hashed
+ * @param isBinaryInput Flag to check  the type provided in agnosticData-parm
  * @returns 
  */
-function hash(agnosticData: any, isBinaryInput: boolean): string {
+export function hash(agnosticData: any, isBinaryInput: boolean): string {
     const buffer: Buffer = !isBinaryInput ? fs.readFileSync(agnosticData) : agnosticData
     const hash: string = sha256(buffer)
     return hash
@@ -33,11 +29,11 @@ function hash(agnosticData: any, isBinaryInput: boolean): string {
  * 
  * @param options Options and data needed to publish a proof of existence
  * @param cb Optional callback-function
- * @returns 
+ * @returns messageId of the message
  */
-async function publishC2(data: string, provider?: string, cb?: Function) {
+export async function publishC2(data: string, provider?: string, cb?: Function) {
+    const config: IConfiguration = require("../options.config");
     if (!provider) {
-        const config: IConfiguration = require("../options.config");
         provider = config.chrysalisOptions.provider;
     }
 
@@ -55,19 +51,13 @@ async function publishC2(data: string, provider?: string, cb?: Function) {
             .accountIndex(0)
             .data(new TextEncoder().encode(data))
 
-        // if (options.tag) {
-        //     msgSender.index(options.tag)
-        // }
-        // if (options.seed) {
-        //     msgSender.seed(options.seed)
-        // }
         const msg = await msgSender.submit()
 
         if (cb) {
             cb(msg)
         }
         else {
-            return msg;
+            return msg.messageId;
         }
     }
     catch (e) {
@@ -75,24 +65,58 @@ async function publishC2(data: string, provider?: string, cb?: Function) {
     }
 }
 
-async function publishLegacy(bundle: ILegacyPublishOptions, cb: Function) {
+/**
+ * 
+ * @param publishOptions Options for publishing, except the data field all values can also be provided via config.
+ * @param cb Optional callback function
+ * @returns Transaction array of of Tangle-Proof of existence
+ */
+export async function publishLegacy(publishOptions: ILegacyPublishOptions, cb?: Function) {
+
+    //Get all parameters
+    const config: IConfiguration = require("../options.config");
+    if (!publishOptions.provider) {
+        publishOptions.provider = (config.legacyOptions?.provider) ? config.legacyOptions?.provider : "";
+    }
+
+    if (!publishOptions.address) {
+        publishOptions.address = (config.legacyOptions?.address) ? config.legacyOptions?.address : "";
+    }
+
+    if (!publishOptions.seed) {
+        publishOptions.seed = (config.legacyOptions?.seed) ? config.legacyOptions?.seed : "";
+    }
+
+    if (!publishOptions.depth) {
+        publishOptions.depth = (config.legacyOptions?.depth) ? config.legacyOptions?.depth : -1;
+    }
+
+    if (!publishOptions.minWeightMagnitude) {
+        publishOptions.minWeightMagnitude = (config.legacyOptions?.minWeightMagnitude) ? config.legacyOptions?.minWeightMagnitude : -1;
+    }
+
+    if(publishOptions.provider === "" || publishOptions.address === "" || publishOptions.seed === "" || publishOptions.depth === -1 || publishOptions.minWeightMagnitude === -1){
+        throw "Did not provide all the parameters needed either through parameters or config!";
+    }
+
     const iota = composeAPI({
-        provider: bundle.provider
+        provider: publishOptions.provider
     })
-    const trytes = asciiToTrytes(bundle.data);
+    const trytes = asciiToTrytes(publishOptions.data);
+
     // Array of transfers which defines transfer recipients and value transferred in IOTAs.
-    const transfers = [{
-        address: bundle.address,
+    const transfers: Transfer[] = [{
+        address: publishOptions.address,
         value: 0, // 1Ki
-        tag: bundle.tag ? bundle.tag.toUpperCase() : '',// optional tag of `0-27` trytes
+        tag: publishOptions.tag ? publishOptions.tag.toUpperCase() : '',// optional tag of `0-27` trytes
         message: trytes // optional message in trytes
     }]
 
     let prepTrytes = null
     let ret = null
     try {
-        prepTrytes = await iota.prepareTransfers(bundle.seed, transfers)
-        ret = await iota.sendTrytes(prepTrytes, bundle.depth, bundle.minWeightMagnitude)
+        prepTrytes = await iota.prepareTransfers(publishOptions.seed, transfers)
+        ret = await iota.sendTrytes(prepTrytes, publishOptions.depth, publishOptions.minWeightMagnitude)
         return ret
     } catch (e) {
         throw `Could not establish a connection to the node ${e}`
@@ -105,7 +129,7 @@ async function publishLegacy(bundle: ILegacyPublishOptions, cb: Function) {
  * @param provider The endpoint to use. If not set, use value from config-file
  * @returns string of actual payload data (e.g. the hash)
  */
-async function fetchC2(messageId: string, provider?: string): Promise<string> {
+export async function fetchC2(messageId: string, provider?: string): Promise<string> {
 
     if (!provider) {
         const config: IConfiguration = require("../options.config");
@@ -137,12 +161,17 @@ async function fetchC2(messageId: string, provider?: string): Promise<string> {
     }
 }
 
-async function fetchLegacy(bundle: ILegacyFetchOptions) {
+/**
+ * 
+ * @param fetchOptions Options for fetching data from the Tangle
+ * @returns The payload of the specified transaction, potentially containing a Proof-of-existence hash
+ */
+export async function fetchLegacy(fetchOptions: ILegacyFetchOptions) {
     const iota = composeAPI({
-        provider: bundle.provider
+        provider: fetchOptions.provider
     })
 
-    const address = bundle.address
+    const address = fetchOptions.address
     let response: Transaction[];
     try {
         // Promise<ReadonlyArray<Transaction>>;
@@ -156,7 +185,7 @@ async function fetchLegacy(bundle: ILegacyFetchOptions) {
     */
     //@ts-ignore
     if (response && typeof response !== "undefined" && response !== '' && response !== undefined) {
-        let asciiArr = response.filter(res => res.hash === bundle.hash)[0]
+        let asciiArr = response.filter(res => res.hash === fetchOptions.hash)[0]
         if (!asciiArr || asciiArr === undefined) {
             throw 'Returned an empty object'
         }
